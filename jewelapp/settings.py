152a6a53e26faadf030e06985ddb683b637fcd4f
MaX <akhiledathadan007@gmail.com>
2020-09-28 11:33:22 +0530

    added pipcache

--- jewelapp/settings.py
+++ jewelapp/settings.py
@@ -89,6 +89,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    },
+    'memory': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
     }
 }
 
