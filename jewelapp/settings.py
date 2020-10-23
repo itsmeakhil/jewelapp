b811f7aba7ebfaf5dabd67a5dca630df2ecb5148
MaX <akhiledathadan007@gmail.com>
2020-10-23 18:34:44 +0530

    updated settings

--- jewelapp/settings.py
+++ jewelapp/settings.py
@@ -20,7 +20,7 @@ ALLOWED_HOSTS = ['*']
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ORIGIN_WHITELIST = ['*']
+CORS_ORIGIN_WHITELIST = ['*', 'https://*', 'http://*']
 
 """
 Uncomment below two lines to use heroku database in local. and change ON_HEROKU to True
